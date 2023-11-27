3438bd256259bc9fbefc767a4a85ddc29c0c1cd9
Sreyasperumal <sreyas2497@gmail.com>
2023-11-27 16:26:55 +0000

    commited

--- server.js
+++ server.js
@@ -16,10 +16,18 @@ app.use(express.json())
 app.use(cookieParser());
 app.use(express.urlencoded({extended: false}));
 app.use(bodyParser.json());
-app.use(cors({ 
-    orgin:["http://localhost:3000","https://invent-app.vercel.app"],
-    credentials:true
-}));
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
+// app.use(cors({ 
+//     orgin:["http://localhost:3000","https://invent-app.vercel.app"],
+//     credentials:true
+// }));
 
 app.use("/uploads",express.static(path.join(__dirname,"uploads")))
 
